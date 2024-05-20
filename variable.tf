48f9cc2183d9abb1cfed3ac10d5c672a69289e24
Dipanjan Biswas <dipanjan.aws.dev@gmail.com>
2024-05-20 20:24:56 +0530

    updated

--- variable.tf
+++ variable.tf
@@ -5,14 +5,17 @@ variable "cluster_name" {
 
 variable "vpc_cidr" {
     type = string
+    default = "10.0.0.0/16"
 }
 
 variable "public_subnet_az1_cidr" {
     type = string
+    default = "10.0.1.0/24"
 }
 
 variable "public_subnet_az2_cidr" {
     type = string
+    default = "10.0.2.0/24"
 }
 
 # Worker Node instance size
